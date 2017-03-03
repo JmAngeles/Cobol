1879ed09203df97c56a5c6b78a0e44b4fc3d0a6a
Jm Angeles <jm.angeles086@gmail.com>
2017-03-03 11:14:05 +0800

    Added Storage Section for Aroma

---
+++ AromaSalesRpt.cbl
@@ -0,0 +1,29 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  InputSort.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       SELECT StudentFile ASSIGN TO "SORTSTUD.DAT"
+       ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT WorkFile ASSIGN TO "WORK.TMP".
+       DATA DIVISION.
+       FILE SECTION.
+
+
+
+       WORKING-STORAGE SECTION.
+       01  Oils-Table.
+           02  Oil-Cost-Values.
+               03 FILLER               PIC X(40)
+                   VALUE "0041003200450050002910250055003900650075".
+               03 FILLER               PIC X(40)
+                   VALUE "0080004400500063006500550085004812500065".
+               03 FILLER               PIC X(40)
+                   VALUE "0060005500670072006501250085006511150105".
+           02  FILLER REDEFINES Oil-Cost-VALUES.
+               03 OIL-COST           PIC 99V99 OCCURS 30 TIMES.
+
+
+
+       PROCEDURE DIVISION.
